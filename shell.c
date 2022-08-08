2a3b778c72503f6aedf01ff8a5c6b6c06e8ffe9f
Tjosh007 <tjosh0077@gmail.com>
2022-08-08 15:31:29 -0700

    commit

---
+++ shell.c
@@ -0,0 +1,61 @@
+#include "shell.h"
+/**
+ * exec - creates a child process in shell
+ * @args: arguments
+ * Return: -1 or nothing
+ */
+int exec(char **argv)
+{
+pid_t child;
+int status;
+char **env = environ;
+if (strcmp("exit", argv[0]) == 0)
+exit(EXIT_SUCCESS);
+child = fork();
+if (child == -1)
+{
+perror("Process failed");
+exit(-1);
+}
+if (child == 0)
+{
+execve(argv[0], argv, env);
+perror("Error");
+}
+else
+{
+wait(&status);
+}
+return (0);
+}
+/**
+ * main - main shell program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, -1 on failure
+ */
+int main(int argc, char **argv)
+{
+size_t n;
+char **tokenize, *buffer;
+int size;
+buffer = NULL;
+(void) argv;
+if (argc < 1)
+return (-1);
+n = 0;
+while (1)
+{
+printf("$: ");
+size = getline(&buffer, &n, stdin);
+if (strcmp("exit\n", buffer) == 0)
+return (-1);
+if (size == -1)
+return (-1);
+if (buffer[size - 1] == '\n')
+buffer[size - 1] = '\0';
+tokenize = tokens(buffer);
+exec(tokenize);
+}
+return (0);
+}
---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <string.h>
+extern char **environ;
+char *_strdup(char *str);
+char **tokens(char *buffer);
+int exec(char **argv);
+#endif
